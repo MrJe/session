427084fc1701b01ae87cd3248674b0e1a8ae87ca
jmichaud <jmichaud@student.42.fr>
2018-06-26 11:19:29 +0200

    reset dir

---
+++ reset/cleanup.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+rm -rf ~/Library/**.42_cache_bak_**
+rm -rf ~/**.42_cache_bak_**
+brew cleanup
---
+++ reset/reset.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+touch ~/.reset
+touch ~/.reset_library
+echo "You can now log out to RESET the session"
