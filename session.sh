26e48f6f3790ce86665fbb2fe3e17c1521c5be38
jmichaud <jmichaud@student.42.fr>
2018-06-27 15:38:05 +0200

    quick

--- session.sh
+++ session.sh
@@ -4,14 +4,23 @@ source ./_omzsh/omzsh.sh
 source ./_reset/reset.sh
 source ./_vim/sublivim.sh
 
+quick_setup() {
+	omzsh ${1}
+	sublivim_install
+}
+
 if [ $# == 0 ]
 then
-	omzsh
-	sublivim_install
+	quick_setup
+elif [ ${1} == "omzsh" ]
+then
+	omzsh ${2}
 elif [ ${1} == "reset" ]
 then
 	reset_session
-else
-	omzsh ${1}
+elif [ ${1} == "sublivim" ]
+then
 	sublivim_install
+else
+	quick_setup ${1}
 fi
